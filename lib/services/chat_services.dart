3debbdb13f7148540ee903cf0c6c1c4e83a65912
Jorsz <jordanjosia929@gmail.com>
2026-02-09 21:56:54 +0800

    Refactor ChatServices to use async methods for message handling and improve Firestore integration

--- lib/services/chat_services.dart
+++ lib/services/chat_services.dart
@@ -1,18 +1,40 @@
 import 'package:winterarc/models/chat_message.dart';
+import 'firestore_service.dart';
 
 class ChatServices {
   final List<ChatMessage> _messages = [];
+  final FirestoreService _firestoreService = FirestoreService();
 
   List<ChatMessage> get messages => List.unmodifiable(_messages);
 
-  void addUserMessage(String text) {
-    _messages.add(
-      ChatMessage(
+  Future<void> loadMessages() async {
+    final loaded = await _firestoreService.loadMessages();
+    _messages.clear();
+    _messages.addAll(loaded);
+  }
+
+  Future<void> addUserMessage(String text) async {
+    final message = ChatMessage(
         id: DateTime.now().millisecondsSinceEpoch.toString(), 
         text: text, 
         sender: MessageSender.user, 
         timestamp: DateTime.now(),
-      ));
+      );
+
+      _messages.add(message);
+      await _firestoreService.saveMesage(message);
+  }
+
+  Future<void> addBotMessage (String text) async {
+    final message = ChatMessage(
+    id: DateTime.now().millisecondsSinceEpoch.toString(), 
+    text: text, 
+    sender: MessageSender.bot, 
+    timestamp: DateTime.now()
+    );
+
+    _messages.add(message);
+    await _firestoreService.saveMesage(message);
   }
 
   ChatMessage createBotReply(String text) {
